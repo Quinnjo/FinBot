ed1cf89efec24c72c0ca9773ee4eefc0db40c531
Squinnjo <Squinnjo@Q>
2018-05-26 16:40:53 -0400

    bot skeleton with test command

--- DiscordBot/src/main/java/Main.java
+++ DiscordBot/src/main/java/Main.java
@@ -1,9 +1,18 @@
+import sx.blah.discord.api.*;
+import sx.blah.discord.util.DiscordException;
 
 public class Main {
-
+	private static final String token = "NDQ5NjkxMjYyNTM5Mzk5MTY4.Des4Bw.m0F8LeLGxD1x_BDqQay6PD5z-o0";
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		IDiscordClient client = BotUtils.getDiscordClient(token);
+		
+		client.getDispatcher().registerListener(new Events());
+		
+		client.login();
 
 	}
+	
 
 }
